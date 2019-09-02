d01f16affae64cd3107dca28c4ede0d25e4fa96e
Pazit Schrecker <38817948+pazitschrecker@users.noreply.github.com>
2019-09-02 11:26:54 -0400

    Delete __init__.py

--- css/bootstrap/flaskblog/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-
-app = Flask(__name__)
-app.config['SECRET_KEY'] = 'cfaa54ba35133fedf8cce33ad2527e68'
-app.config['SQLALCHEMY_DATABASE_URL'] = 'sqlite:///site.db'
-db = SQLAlchemy(app)
-
-from flaskblog import routes
